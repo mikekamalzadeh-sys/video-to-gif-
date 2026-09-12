2fde207cb928a094b4f61d8eedd8df3c110fce2b
mikekamalzadeh-sys <mikekamalzadeh@gmail.com>
2026-09-12 14:18:46 +0330

    Update price_bot_lite-6.py

--- price_bot_lite-6.py
+++ price_bot_lite-6.py
@@ -42,7 +42,7 @@ BASE_URL = f"https://api.splus.ir/bot{BOT_TOKEN}"
 
 # کلید رایگان BrsApi (برای دلار و طلای ۱۸ عیار)
 # ثبت‌نام رایگان: https://brsapi.ir/free-api-gold-currency-webservice/
-BRSAPI_KEY = "BmheKA6XdLbzSL9YyGSgwzrdwqssQztK"
+BRSAPI_KEY = "Bs9BZaDzfZLCakW65QEKKDNFdttG5qZ6"
 BRSAPI_URL = "https://Api.BrsApi.ir/Market/Gold_Currency.php"
 
 # API عمومیِ آمار بازار نوبیتکس - نیازی به کلید ندارد
